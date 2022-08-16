6c1871281c3efb4df2fd7cade2fe3a2c71cdf9b7
Еlena Anisimova <elena.anis.12@yandex.ru>
2022-08-16 15:46:28 +0300

    feat: add footer and background

---
+++ momentum/js/name.js
@@ -0,0 +1,13 @@
+const name = document.querySelector(".name");
+
+export function setLocalStorage() {
+  localStorage.setItem("name", name.value);
+}
+window.addEventListener("beforeunload", setLocalStorage);
+
+export function getLocalStorage() {
+  if (localStorage.getItem("name")) {
+    name.value = localStorage.getItem("name");
+  }
+}
+window.addEventListener("load", getLocalStorage);
